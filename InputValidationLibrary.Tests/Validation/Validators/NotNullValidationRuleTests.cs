854dcbf96707487b09acdcafa813b70a925acae5
LuigimonSoft <almadejaguar@hotmail.com>
2024-10-11 23:15:09 -0400

    Fix position on validators and first test

---
+++ InputValidationLibrary.Tests/Validation/Validators/NotNullValidationRuleTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InputValidationLibrary.Validation.Validators;
+using InputValidationLibrary.Validation;
+using Syste.Collections.Generic;
+
+namespace InputValidationLibrary.Tests.Validation.Validators
+{
+  [TestClass]
+  public class NotNullValidationRuleTests
+  {
+    private NotNullValidationRule<TestObject, string> _rule;
+    private Dictionary<int, string> _errorMessages;
+
+    [TestInitialize]
+    public void Setup()
+    {
+      _rule = new NotNullValidationRule<TestObject, string>(x => x.Value);
+      _errorMessages = new Dictionary<int, string> {{1001, "Value cannot be null."}};
+      _rule.ErrorCode = 1001;
+    }
+
+    [TestMethod]
+    public void Validate_WhenValueIsNotNull_ShouldPass()
+    {
+      // Arrange
+      var testObject = new TestObject {Value = "test"};
+      var result = new ValidationResult();
+
+      // Act
+      _rule.Validate(testObject, _errorMessages);
+
+      // Assert
+      Assert.IsTrue(result.IsValid);
+    }
+  }
+
+  private class TestObject
+  {
+    public string Value { get; set; }
+  }
+}
