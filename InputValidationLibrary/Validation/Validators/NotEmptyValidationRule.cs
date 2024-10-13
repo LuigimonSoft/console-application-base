25721fa3ea44f025eece4c9b14d36e5935b39491
Luis Carlos <almadejaguar@hotmail.com>
2024-10-13 11:10:51 -0400

    Add NotEmptyValidationRule.cs
    Add not empty validation rule

---
+++ InputValidationLibrary/Validation/Validators/NotEmptyValidationRule.cs
@@ -0,0 +1,33 @@
+using System;
+using InputValidationLibrary.Validation.Interfaces;
+
+namespace InputValidationLibrary.Validation.Validators
+{
+    public class NotEmptyValidationRule<T> : IValidationRule<T>
+    {
+        private readonly Func<T, string> _property;
+        public int? ErrorCode { get; set; }
+
+        public NotEmptyValidationRule(Func<T, string> property)
+        {
+            _property = property;
+        }
+
+        public void Validate(T entity, ValidationResult result, Dictionary<int, string> errorMessages)
+        {
+            var value = _property(entity);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                if (ErrorCode.HasValue && errorMessages.TryGetValue(ErrorCode.Value, out var errorMessage))
+                {
+                    result.AddError(errorMessage);
+                }
+                else
+                {
+                    result.AddError("The value must not be empty or consist solely of whitespace.");
+                }
+            }
+        }
+    }
+}
