b52493dce23e3f540cb23f97d8329521e50aa7ae
Luis Carlos <almadejaguar@hotmail.com>
2024-10-13 19:00:30 -0400

    Add model TestModel.cs
    Add test model

---
+++ InputValidationLibrary.Tests/Models/TestModel.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace InputValidationLibrary.Models
+{
+    public class TestModel
+    {
+        [Column(0)]
+        public string Name { get; set; }
+
+        [Column(1)]
+        public int Age { get; set; }
+
+        [Column(2)]
+        public decimal Salary { get; set; }
+
+        [Column(3)]
+        public string FilePath { get; set; }
+    }
+}
