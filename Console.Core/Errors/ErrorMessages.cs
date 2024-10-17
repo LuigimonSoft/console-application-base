a86acfc6a6b21ecac7a3205fb486360d3a8a84ee
LuigimonSoft <almadejaguar@hotmail.com>
2024-10-16 23:59:39 -0400

    FIx name Dictionary

--- Console.Core/Errors/ErrorMessages.cs
+++ Console.Core/Errors/ErrorMessages.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 public static class ErrorMessages
 {
-    public static dictionary<string, string> ErrorList = new Dictionary<string, string>
+    public static Dictionary<string, string> ErrorList = new Dictionary<string, string>
     {
         { 1001, "Name must not be null." },
         { 1002, "Name must not be empty." },
